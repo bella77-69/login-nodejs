5ff69d64abf6ffde0f6a04c6ff68249b5a8f33e8
chantelle mckenzie <mrspasceri@hotmail.com>
2022-01-20 22:14:04 -0800

    successfully set up and tested api routes using passport and jsonwebtoken for auth.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const passport = require('passport');
+const users = require('./routes/api/users')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false})
@@ -14,6 +15,15 @@ mongoose.connect(db, { useNewUrlParser: true }
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport config
+require('./config/passport')(passport);
+
+//Routes
+app.use('/api/users', users);
 
 const PORT = process.env.PORT || 5000; 
 
